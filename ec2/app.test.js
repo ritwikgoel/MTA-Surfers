c913d158d230544f6a64b88346afc08c30d64803
17PZ <145883663+17PZ@users.noreply.github.com>
2023-12-16 17:40:43 -0500

    Add files via upload

--- ec2/app.test.js
+++ ec2/app.test.js
@@ -1,16 +1,25 @@
 const request = require('supertest');
-const app = require('./app'); 
+const express = require('express');
+
+// Mock
+const app = express();
+app.get('/api/users', (req, res) => {
+  res.json([{ id: 1, name: 'John Doe' }]);
+});
+
 describe('GET /api/users', () => {
   it('should respond with a list of users', async () => {
     const res = await request(app)
-      .get('/api/users')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200);
+        .get('/api/users')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200);
 
-    // Add more assertions 
     expect(res.body).toBeInstanceOf(Array);
+    expect(res.body.length).toBeGreaterThan(0);
+    expect(res.body[0]).toHaveProperty('id');
+    expect(res.body[0]).toHaveProperty('name');
   });
-
-  // Add more tests 
 });
+
+
